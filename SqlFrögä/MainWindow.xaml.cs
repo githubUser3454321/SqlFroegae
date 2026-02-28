c930026dce89d0ac939d5438ae3fdd449ff97992
githubUser3454321 <116742137+githubUser3454321@users.noreply.github.com>
2026-02-28 15:23:32 +0100

    Set Windows app icon to SqlFrögä.ico

--- SqlFrögä/MainWindow.xaml.cs
+++ SqlFrögä/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
+using System.IO;
+using Microsoft.UI.Windowing;
 using Microsoft.UI.Xaml;
 using SqlFroega.Views;
+using WinRT.Interop;
 
 namespace SqlFroega;
 
@@ -9,5 +12,19 @@ public sealed partial class MainWindow : Window
     {
         InitializeComponent();
         RootFrame.Navigate(typeof(LibrarySplitView));
+        SetWindowIcon();
+    }
+
+    private void SetWindowIcon()
+    {
+        var hWnd = WindowNative.GetWindowHandle(this);
+        var windowId = Microsoft.UI.Win32Interop.GetWindowIdFromWindow(hWnd);
+        var appWindow = AppWindow.GetFromWindowId(windowId);
+
+        var iconPath = Path.Combine(AppContext.BaseDirectory, "Assets", "SqlFrögä.ico");
+        if (File.Exists(iconPath))
+        {
+            appWindow.SetIcon(iconPath);
+        }
     }
 }
